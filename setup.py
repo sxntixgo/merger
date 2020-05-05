ba46991b3c3b77104feeb7a771f7859c1681b739
sxntixgo <18380937+sxntixgo@users.noreply.github.com>
2020-05-04 21:07:22 -0600

    updating setup script

--- setup.py
+++ setup.py
@@ -6,41 +6,32 @@ import string
 import sys
 
 def usage():
-    print('Usage: %s [-d | -v]')
+    print('Usage: %s [-v]' % sys.argv[0])
+    print('%s generates .env file for docker setup' % sys.argv[0])
     print('Options')
-    print('-d\t: generate .env file for docker setup. Default.')
-    print('-v\t: gemerate .env file for venv setup.')
+    print('-v\t: generate .env file for venv setup instead.')
 
 def main():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'dhv', ['--help'])
+        opts, args = getopt.getopt(sys.argv[1:], 'v')
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)
         usage()
         sys.exit(2)
-    
 
-    for o, a in opts:
-        if o in ('-d', '-v'):
-            dictionary = string.ascii_letters + string.digits + string.punctuation
-            db_pass = ''.join(secrets.choice(dictionary) for i in range(64))
-            web_pass = ''.join(secrets.choice(dictionary) for i in range(64))
-            with open('.env', 'w') as f:
-                f.write('DB_PASSWORD=' + db_pass + '\n')
-                f.write('WEB_PASSWORD=' + web_pass + '\n')
-                if o == '-v':
-                    f.write('DB_ADDRESS=localhost' + '\n')
-                else:
-                    f.write('DB_ADDRESS=db' + '\n')
-            print('.env file created')
+    dictionary = string.ascii_letters + string.digits + string.punctuation
+    db_pass = ''.join(secrets.choice(dictionary) for i in range(64))
+    web_pass = ''.join(secrets.choice(dictionary) for i in range(64))
+
+    with open('.env', 'w') as f:
+        f.write('DB_PASSWORD=' + db_pass + '\n')
+        f.write('WEB_PASSWORD=' + web_pass + '\n')
+        for o, a in opts:
             if o == '-v':
-                print('Execute "for i in `cat .env`; do export $i; done"')
-        elif o in ('-h', '--help'):
-            usage()
-            sys.exit()
-        else:
-            assert False, 'unhandled option'
+                    f.write('DB_ADDRESS=localhost' + '\n')
+            else:
+                    f.write('DB_ADDRESS=db' + '\n')
 
 if __name__ == "__main__":
     main()
